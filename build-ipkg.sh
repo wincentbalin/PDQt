b12da0274f546be9ec987c81993a75952dc789cc
wincent <wincent@7f5ff683-dd5d-49d8-9ff2-f30a9a231772>
2009-01-11 19:18:40 +0000

    Added .ipk package creation files.
    git-svn-id: https://svn.katastrophos.net/pdqt/trunk@23 7f5ff683-dd5d-49d8-9ff2-f30a9a231772

---
+++ build-ipkg.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Build ipk of pdqt
+
+# Pack control file
+tar cvf control.tar ./control
+gzip -fv9 control.tar
+
+# Prepare and copy pdqt binary
+arm-linux-strip pdqt
+cp pdqt ./opt/QtPalmtop/bin/
+
+# Pack package data
+tar cvf data.tar ./opt --exclude .svn
+gzip -fv9 data.tar
+
+# Pack the ipk package
+tar zcvf pdqt_0.5_arm.ipk ./control.tar.gz ./data.tar.gz ./debian-binary
+
+# Clean up
+rm data.tar* control.tar* 
