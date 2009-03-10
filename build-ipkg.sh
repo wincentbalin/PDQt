714e4c09f39c981fa0de8e54c3b2c71b71cb3b65
wincent <wincent@7f5ff683-dd5d-49d8-9ff2-f30a9a231772>
2009-03-10 11:39:21 +0000

    Create control file on the fly. Also removes problems with different versions in the control file and the package name.
    git-svn-id: https://svn.katastrophos.net/pdqt/trunk@32 7f5ff683-dd5d-49d8-9ff2-f30a9a231772

--- build-ipkg.sh
+++ build-ipkg.sh
@@ -4,9 +4,24 @@
 
 VERSION=0.6
 
+# Create control file
+echo -n > control
+echo "Package: pdqt"                             >> control
+echo "Version: $VERSION"                         >> control
+echo "Section: multimedia"                       >> control
+echo "Priority: optional"                        >> control
+echo "Architecture: arm"                         >> control
+echo "Depends: none"                             >> control
+echo "Maintainer: Wincent Balin"                 >> control
+echo "Source: http://trac.katastrophos.net/PDQt" >> control
+echo "Description: Qtopia based re-creation of the iPxd front-end for Pure Data" >> control
+
 # Pack control file
 tar cvf control.tar ./control
 gzip -fv9 control.tar
+
+# Remove control file, it is not needed anymore
+rm control
 
 # Prepare and copy pdqt binary
 arm-linux-strip pdqt
@@ -22,3 +37,4 @@ tar zcvf pdqt_"$VERSION"_arm.ipk ./control.tar.gz ./data.tar.gz ./debian-binary
 # Clean up
 rm data.tar* control.tar* 
 rm opt/QtPalmtop/bin/pdqt
+
